d28cb98f1e87d73f922deb49d4f807a08266d8fa
mitubaEX <i1788188@cse.kyoto-su.ac.jp>
2017-12-01 05:45:55 -0800

    fix config.fish Ctrl-f complete

--- config.fish
+++ config.fish
@@ -45,3 +45,10 @@ alias rm='rmtrash'
 function hello
     echo 'hello'
 end
+
+# ctrl-fを使えるようにする
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
