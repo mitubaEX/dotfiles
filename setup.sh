7b1d0d3cc3e113fe45fe5d16f4c3e8d6a53a8947
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-09-28 21:01:08 +0900

    add mkdir

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 ln -sf $(pwd)/dein.toml $HOME/dein.toml
+
+mkdir $HOME/.config/nvim
 ln -sf $(pwd)/init.vim $HOME/.config/nvim/init.vim
+
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
 ln -sf $(pwd)/.zpreztorc $HOME/.zpreztorc
 ln -sf $(pwd)/.tmux.conf.local $HOME/.tmux.conf.local
+
+mkdir $HOME/.config/alacritty
 ln -sf $(pwd)/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 mkdir $HOME/.config/i3
