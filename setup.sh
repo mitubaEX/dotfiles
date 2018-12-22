5aafab132e715f7362ee2289ecf196be3e073d95
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-12-23 00:41:58 +0900

    mkdir .config dir and move each config file.

--- setup.sh
+++ setup.sh
@@ -3,17 +3,17 @@ mkdir $HOME/.config
 ln -sf $(pwd)/dein.toml $HOME/dein.toml
 
 mkdir $HOME/.config/nvim
-ln -sf $(pwd)/init.vim $HOME/.config/nvim/init.vim
+ln -sf $(pwd)/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
 ln -sf $(pwd)/.zpreztorc $HOME/.zpreztorc
 ln -sf $(pwd)/tmux/.tmux.conf $HOME/.tmux.conf
 
 mkdir $HOME/.config/alacritty
-ln -sf $(pwd)/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+ln -sf $(pwd)/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 mkdir $HOME/.config/i3
-ln -sf $(pwd)/config $HOME/.config/i3/config
+ln -sf $(pwd)/.config/i3/config $HOME/.config/i3/config
 
 ln -sf $(pwd)/.xintrc $HOME/.xintrc
 ln -sf $(pwd)/.xprofile $HOME/.xprofile
