a2bff4094effb8d64a054ad2703fc9ec3b70c8aa
mitubaEX <jun-nakamura@c-fo.com>
2019-08-23 02:09:23 +0900

    fix config for ec2

--- .zsh.d/utils/env-tools.zsh
+++ .zsh.d/utils/env-tools.zsh
@@ -2,5 +2,8 @@
 export EDITOR=nvim
 eval "$(direnv hook zsh)"
 
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init -)"
+
 eval "$(nodenv init -)"
 eval "$(rbenv init -)"
--- .zsh.d/utils/zplugins.zsh
+++ .zsh.d/utils/zplugins.zsh
@@ -1,5 +1,5 @@
 ### Added by Zplugin's installer
-source '/Users/nakamura-jun/.zplugin/bin/zplugin.zsh'
+source $HOME/.zplugin/bin/zplugin.zsh
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 ### End of Zplugin's installer chunk
