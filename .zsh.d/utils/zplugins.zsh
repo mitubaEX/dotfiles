5eedae2f91f35a2b570b3dc8c52df3e40972dd05
mitubaEX <jun-nakamura@c-fo.com>
2019-08-23 20:57:42 +0900

    fix zsh config

--- .zsh.d/utils/zplugins.zsh
+++ .zsh.d/utils/zplugins.zsh
@@ -11,22 +11,3 @@ zplugin light zdharma/fast-syntax-highlighting
 # Load the pure theme, with zsh-async library that's bundled with it.
 zplugin ice pick"async.zsh" src"pure.zsh"
 zplugin light sindresorhus/pure
-
-# Binary release in archive, from GitHub-releases page.
-# After automatic unpacking it provides program "fzf".
-zplugin ice from"gh-r" as"program"
-zplugin load junegunn/fzf-bin
-
-# One other binary release, it needs renaming from `docker-compose-Linux-x86_64`.
-# This is done by ice-mod `mv'{from} -> {to}'. There are multiple packages per
-# single version, for OS X, Linux and Windows – so ice-mod `bpick' is used to
-# select Linux package – in this case this is actually not needed, Zplugin will
-# grep operating system name and architecture automatically when there's no `bpick'.
-zplugin ice from"gh-r" as"program" mv"docker* -> docker-compose" bpick"*linux*"
-zplugin load docker/compose
-
-# Scripts that are built at install (there's single default make target, "install",
-# and it constructs scripts by `cat'ing a few files). The make'' ice could also be:
-# `make"install PREFIX=$ZPFX"`, if "install" wouldn't be the only, default target.
-zplugin ice as"program" pick"$ZPFX/bin/git-*" make"PREFIX=$ZPFX"
-zplugin light tj/git-extras
--- .zshenv
+++ .zshenv
@@ -1,5 +1,6 @@
 # check performance
 # zmodload zsh/zprof && zprof
 
+export HISTFILE=${HOME}/.zsh_history
 export ZDOTDIR=$HOME/.zsh.d
 source $ZDOTDIR/.zshenv
