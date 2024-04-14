e50d569df7e1bfc96d0bf638819c2f4f2e76acbc
mitubaEX <jun-nakamura@c-fo.com>
2024-04-14 21:19:33 +0900

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'nightfox'
+config.window_decorations = 'RESIZE'
+config.enable_tab_bar = false
+config.font = wezterm.font_with_fallback({
+  { family = 'JetBrains Mono', harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, },
+  { family = 'JetBrains Mono', assume_emoji_presentation = true },
+})
+config.font_size = 13.0
+
+config.use_ime = true
+config.macos_forward_to_ime_modifier_mask = "CTRL"
+
+-- and finally, return the configuration to wezterm
+return config
